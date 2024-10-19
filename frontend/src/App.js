e42ea39fd7608de0dabdfe232983704b15e30874
Gruppo23-code <gruppo39poliba@gmail.com>
2024-10-19 10:27:03 +0200

    Allineamento con repository di test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,10 +4,10 @@ import {
 } from "react-router-dom";
 
 import Registrazione from "./pages/registrazione.jsx";
-import Dashboard from "./pages/dashboard.jsx";
-import Guide from "./pages/guide.jsx";
-import Marketplace from "./pages/marketplace.jsx";
-import Community from "./pages/community.jsx";
+//import Dashboard from "./pages/dashboard.jsx";
+//import Guide from "./pages/guide.jsx";
+//import Marketplace from "./pages/marketplace.jsx";
+//import Community from "./pages/community.jsx";
 
 import Navbar from "./Components/navbar.js";
 
@@ -20,22 +20,6 @@ const router = createBrowserRouter([
     {
         path: "/registrazione",
         element: <Registrazione/>,
-    },
-    {
-        path: "/dashboard",
-        element: <Dashboard/>,
-    },
-    {
-        path: "/guide",
-        element: <Guide/>,
-    },
-    {
-        path: "/marketplace",
-        element: <Marketplace/>,
-    },
-    {
-        path: "/community",
-        element: <Community/>,
     },
 ]);
 
