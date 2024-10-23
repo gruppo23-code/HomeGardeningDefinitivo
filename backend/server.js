373897b7ef034d560f412ae60f8f29bb924cbb4c
Gruppo23-code <gruppo39poliba@gmail.com>
2024-10-23 23:59:30 +0200

    Login funzionante, da completare

--- backend/server.js
+++ backend/server.js
@@ -24,13 +24,14 @@ app.get('/', (req, res) => {
     res.send('Benvenuto alla homepage!');
 });
 
+//Funzione per la registrazione
 app.post('/registrazione', (req, res) => {
     const sql = `INSERT INTO  utenti (nome,cognome,email,password) VALUES (?)`;
 
     const saltRounds = 10; // Maggiore è il numero, più sicuro sarà l'hashing, ma richiederà più tempo
+    //Commentato per utilizzo di variabili d'ambiente
 
-
-    bcrypt.hash(req.body.password.toString(),saltRounds, (err,hash) => {
+    bcrypt.hash(req.body.password.toString(), saltRounds, (err, hash) => {
         if (err) {
             console.error("Errore durante l'hashing della password: ", err);
         }
@@ -41,11 +42,11 @@ app.post('/registrazione', (req, res) => {
             hash,
         ]
         connessione.query(sql, [values], (err, result) => {
-            console.log(result);
+            //console.log(result);
             if (err) {
                 if (err.code === 'ER_DUP_ENTRY') {
                     //Ritorno il codice ER_DUP_ENTRY al frontend
-                    return res.status(409).json({ error: err.code });
+                    return res.status(409).json({error: err.code});
                 } else {
                     console.error("Errore durante l'esecuzione della query: ", err);
                 }
@@ -56,7 +57,43 @@ app.post('/registrazione', (req, res) => {
     })
 })
 
+//Funzione per il login
+app.post('/login', (req, res) => {
+    const query = 'SELECT password FROM utenti WHERE email = ?';
+    const email = req.body.email;
 
-app.listen(8081, () => {
+
+    connessione.query(query, [email], (err, result) => {
+        if (err) {
+            console.error("Errore durante la query: ", err);
+            return result.status(500).send("Errore del server");
+        }
+        if (result.length === 0) {
+            console.log(result)
+            return res.status(404).send("Utente non trovato");
+        } else if (result.length === 1) {
+            bcrypt.compare(req.body.password, result[0].password, (err, match) => {
+                //.compare serve a comparare le password, facendo automaticamente l'hash della password che passo
+                //dal frontend; ritorna una variabile che ho chiamato match, boolean quindi true se matchano altrimenti false
+                if (err) {
+                    console.error("Errore durante il confronto delle password: ", err);
+                    return res.status(500).json({error: err});
+                }
+                if (match) {
+                    console.log("Le password matchano!");
+                    return res.status(200).send("SUCCESSO");
+                } else {
+                    console.log("Le password non matchano!");
+                    return res.status(401).json({error: err});
+                }
+            })
+        }
+    })
+
+
+})
+
+
+app.listen(process.env.LISTEN_PORT || 8081, () => {
     console.log('Running...');
 })
--- frontend/src/Components/login_form.jsx
+++ frontend/src/Components/login_form.jsx
@@ -1,14 +1,47 @@
-import React from "react";
+import React, {useRef} from "react";
+import axios from "axios";
 
-function LoginForm(props) {
+function LoginForm() {
+    const alertRef = useRef(null);
+    const [alert, setAlert] = React.useState("invisible");
+
+    const [valori, setValori] = React.useState({
+        email: '',
+        password: '',
+    });
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axios.post('http://localhost:8081/login', valori)
+            .then(res => {
+                //Ritorno la stringa "SUCCESSO" nel backend
+                alert(res.data);
+                //console.log(res.data);
+            })
+            .catch(err => {
+                //Catturo il codice di errore 401
+                //Errore 401 = Unauthorized: L'accesso alla risorsa è negato per mancanza di credenziali valide.
+                if (err.response.status === 401 || err.response.status === 404) {
+                    setAlert("visible");
+                }
+            })
+    }
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <div className="form-group align-content-center d-flex flex-column align-items-center">
                 <div className="mt-1 w-auto">
-                    <input type="email" className="form-control" id="InputEmail" aria-describedby="emailHelp" placeholder="Email" />
+                    <input type="email" className="form-control" id="InputEmail" aria-describedby="emailHelp"
+                           placeholder="Email"
+                           onChange={e => setValori({...valori, email: e.target.value})} required/>
                 </div>
                 <div className="mt-3 w-auto">
-                    <input type="password" className="form-control" id="InputPassword" placeholder="Password"/>
+                    <input type="password" className="form-control" id="InputPassword" placeholder="Password"
+                           onChange={e => setValori({...valori, password: e.target.value})} required/>
+                </div>
+                <div  className={alert} >
+                    <div className="alert alert-danger mt-2" role="alert" style={{ fontSize: '12px'}} ref={alertRef}>
+                        Password o email errate!!!
+                    </div>
                 </div>
                 <div className="mt-3 w-auto">
                     <button type="submit" className="btn btn-success">Accedi</button>
