19b2988375ef4a81e76b4267e4af4d9e73b48295
Gogiga-Gagagigo2950 <giuliogatta11@gmail.com>
2024-12-04 11:59:24 +0100

    graffa

--- frontend/src/Components/alert.jsx
+++ frontend/src/Components/alert.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 function Alert({ message }) {
     return (
-        <div className="alert alert-danger mt-2" role="alert" style={{ fontSize: '12px' }}>
+        <div className="alert alert-danger mt-2" role="alert" style={{fontSize: '12px'}}>
             {message}
         </div>
     )
-
+}
 
 export default Alert;
