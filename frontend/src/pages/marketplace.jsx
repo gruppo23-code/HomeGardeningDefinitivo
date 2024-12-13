5ad4324f872985863a0692c713454171e2017876
Gruppo23-code <gruppo39poliba@gmail.com>
2024-12-13 15:02:39 +0100

    Piccoli cambiamenti market

--- frontend/src/pages/marketplace.jsx
+++ frontend/src/pages/marketplace.jsx
@@ -1,6 +1,8 @@
 // src/components/Marketplace.js
 import React, { useState } from "react";
 import './css/marketplace.css';
+import Cookies from "js-cookie";
+import {Link} from "react-router-dom";
 
 function Marketplace() {
     const [searchTerm, setSearchTerm] = useState("");
@@ -47,8 +49,17 @@ function Marketplace() {
         item.name.toLowerCase().includes(searchTerm.toLowerCase())
     );
 
+    const isLoggedIn = () => {
+        const token = Cookies.get('token'); // Sostituisci 'token' con il nome del tuo cookie
+        return !!token; // Ritorna true se il token esiste, false altrimenti
+    };
+
+    const alertClick = () => {
+        alert("Accesso Negato. Devi essere loggato per fare acquisti.");
+    };
+
     return (
-        <div className="marketplace">
+        <div className="marketplace mt-3 mb-3">
             <h1>Marketplace di Giardinaggio</h1>
             <div className="search-container">
                 <input type="text" className="search-input" placeholder="Cerca..."/>
@@ -58,14 +69,18 @@ function Marketplace() {
             </div>
             <div className="item-list">
             {filteredItems.map(item => (
-                    <div key={item.id} className="item">
-                        <img src={item.image} alt={item.name} className="item-image"/>
-                        <h2>{item.name}</h2>
-                        <p className="item-description">{item.description}</p>
-                        <p className="item-price">Prezzo: {item.price}€</p>
+                <div key={item.id} className="item">
+                    <img src={item.image} alt={item.name} className="item-image"/>
+                    <h2>{item.name}</h2>
+                    <p className="item-description">{item.description}</p>
+                    <p className="item-price">Prezzo: {item.price}€</p>
+                    {isLoggedIn() ? (
                         <button className="buy-button">Acquista</button>
-                    </div>
-                ))}
+                    ) : (
+                        <button className="buy-button" onClick={alertClick}>Acquista</button>
+            )}
+            </div>
+            ))}
             </div>
         </div>
     );
